b5831326b8555d58dea90af23695dcbeb3707e76
Robert Alestra <bobbyalestra135@gmail.com>
2020-07-28 20:09:58 -0400

    made another loop that converts an array to a string, puts it in alphabetical order, and logs all the names that contain a B in them

---
+++ logic12.js
@@ -0,0 +1,21 @@
+
+// console logs all the names that have a letter B in them
+
+let names = ['Bobby', 'Brittany',  'Brett', "Bibble", 'John', "Arnell", "will"].sort();
+
+
+for (let i = 0; i < names.length; i++){
+    for ( let j = 0; j <names[i].length; j++){
+  
+        let stringWithOutB = [];
+        let namesToString = names[i].toString().toLowerCase()
+        let indexB = namesToString.indexOf('b') > -1;
+            if ( indexB === true){
+                stringWithOutB.push(namesToString)
+                console.log(stringWithOutB)
+            }
+    }
+}
+
+
+
