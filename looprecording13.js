c8e65efcdcb63360583414bbd99d8bc31f98c4de
Robert Alestra <bobbyalestra135@gmail.com>
2020-08-04 22:55:43 -0400

    more loop recordings

---
+++ looprecording13.js
@@ -0,0 +1,12 @@
+
+
+let numbersArray = [ [1234, 764, 2345,  456456], [7548, 087568, 24786, 234547], [3456876, "a String", 2345, 98765] ];
+
+for (let i = 0; i <numbersArray.length; i++){
+    for (let j = 0; j < numbersArray[i].length; j++){
+
+        console.log(numbersArray[i][i])
+
+
+    }
+}
