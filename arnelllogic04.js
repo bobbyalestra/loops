fe0527c69d627c3472a3a82db177e399a0ffc927
Robert Alestra <bobbyalestra135@gmail.com>
2020-08-19 16:20:48 -0400

    arnells homework

--- arnelllogic04.js
+++ arnelllogic04.js
@@ -15,7 +15,7 @@
 // add those numbers together to get the total
 //then add the second and third numbers for a total and so on
 // use and if statement to set a condition so that if number 1 is > number two it will select the larger sum
- 
+// i need to set a variable += highest total
 
 
 
@@ -23,29 +23,40 @@
 
 
 let numbersArray = [358276442183, 355932218848, 3554259136937, 5602212994565, 353022164588, 639921161300, 56022354427689,51087587653044, 6333641549180, 560224431936850, 5020192861974, 676324012630737, 3530627464605, 63043786670877, 372301634211413 ];
-
+let groupNumbers = [];
+let numTotalArray = []
 // adding a 1 to the initializer instead of zero doeesnt log the first sub array twice
 // adding a -1 to the conditions will prevent it from log undefined and NaN at the last log
 
-for (let i = 1; i < numbersArray.length -1; i++){
+for (let i = 0; i < numbersArray.length - 1; i++){
     
         let number1 = numbersArray[i];
         let number2 = numbersArray[i + 1];
         let numTotal = number1 + number2;
+        let highestTotal =  numTotal = numTotal + 1;
+        let otherOption = numTotal += numTotal + 1;
+       
 
-        let arrayNew = []
-        arrayNew.push(number1, number2, numTotal);
+        groupNumbers.push(number1, number2, numTotal);
+        numTotalArray.push(numTotal);
 
-        // this will log all number1, number2, numTotal
-        console.log(arrayNew);
+        highestTotal = highestTotal + 1
+
+        if (highestTotal < highestTotal + 1){
+
+          
+
+        console.log(highestTotal);
+        console.log("")
+        //console.log(otherOption);
+        }
+
+            // gives me each total
+          // console.log(numTotal)
+
         
         // this will log the first number1, number2, numTotal
-        console.log (arrayNew[i]);
-        
+     // console.log (groupNumbers);
+      }
+    
 
-        if ( arrayNew[i] > arrayNew[i + 1]){
-
-            // this will log the first number1, number2, numTotal
-          console.log (arrayNew[i])
-        }
-    }
