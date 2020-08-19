e39490034ea62505c1999f4cdfcdad077c6e55ef
Robert Alestra <bobbyalestra135@gmail.com>
2020-08-19 19:23:42 -0400

    more loops

--- arnelllogic04.js
+++ arnelllogic04.js
@@ -14,6 +14,7 @@
 // create two variables that equal the first and second number
 // add those numbers together to get the total
 //then add the second and third numbers for a total and so on
+
 // use and if statement to set a condition so that if number 1 is > number two it will select the larger sum
 // i need to set a variable += highest total
 
@@ -28,35 +29,34 @@ let numTotalArray = []
 // adding a 1 to the initializer instead of zero doeesnt log the first sub array twice
 // adding a -1 to the conditions will prevent it from log undefined and NaN at the last log
 
-for (let i = 0; i < numbersArray.length - 1; i++){
+for (let i = 1; i < numbersArray.length - 1; i++){
     
         let number1 = numbersArray[i];
         let number2 = numbersArray[i + 1];
         let numTotal = number1 + number2;
-        let highestTotal =  numTotal = numTotal + 1;
-        let otherOption = numTotal += numTotal + 1;
+        let highestTotal =  numTotal += numTotal + 1;
+  
        
 
         groupNumbers.push(number1, number2, numTotal);
         numTotalArray.push(numTotal);
 
-        highestTotal = highestTotal + 1
+        
+        console.log(typeof(numTotalArray) , "");
 
-        if (highestTotal < highestTotal + 1){
+        if (highestTotal < highestTotal + 1 ){
 
-          
+            highestTotal = highestTotal + 1;
 
-        console.log(highestTotal);
-        console.log("")
-        //console.log(otherOption);
+       
+       
         }
 
             // gives me each total
-          // console.log(numTotal)
+           //console.log(numTotal)
 
         
-        // this will log the first number1, number2, numTotal
-     // console.log (groupNumbers);
+        
       }
     
 
